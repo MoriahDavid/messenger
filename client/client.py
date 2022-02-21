ef75583bd4adeab41ea1d5acf57be581d935eaee
MoriahDavid <moriah.david24@gmail.com>
2022-02-21 13:15:58 +0200

    create client class

---
+++ client/client.py
@@ -0,0 +1,25 @@
+
+class Client:
+    def __init__(self):
+        pass
+
+    def connect(self, ip, port):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def send_msg(self, msg, to=None):
+        pass
+
+    def get_all_clients(self):
+        pass
+
+    def get_all_files(self):
+        pass
+
+    def _file_download_req(self):
+        pass
+
+    def file_download(self, file_name):
+        pass
