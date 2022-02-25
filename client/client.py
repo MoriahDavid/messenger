6ec9efd39f6b7c1bd3bb366b35ca9d6656bab29a
MoriahDavid <moriah.david24@gmail.com>
2022-02-25 13:18:19 +0200

    implement connect method

--- client/client.py
+++ client/client.py
@@ -1,27 +1,64 @@
+import json
+import socket
 from consts import MsgTypes, MsgKeys
+import consts
 
 
 class Client:
     def __init__(self):
-        pass
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self, ip, port):
-        pass
+    def connect(self, username, server_ip, server_port):
+        """
+        create connection to the server with username.
+        :param username:
+        :param server_ip:
+        :param server_port:
+        :return:
+        """
+        self.sock.connect((server_ip, server_port))  # connect to the server
+        d = {MsgKeys.TYPE: MsgTypes.CONNECT, MsgKeys.USERNAME: username}  # create dict (JSON) to the msg
+        self.sock.send(b""+json.dumps(d))  # send the msg by change the JSON to string
+        m = self.sock.recv(consts.BUFF_SIZE)
+        d = json.loads(m)
+        if d.get(MsgKeys.TYPE) == MsgTypes.CONNECT_SUCCESS:
+            print("connected")
+            return True
+        self.sock.close()
+        return False
 
     def disconnect(self):
-        pass
+        d = {MsgKeys.TYPE: MsgTypes.DISCONNECT}
+        self.sock.send(b"" + json.dumps(d))
+        self.sock.close()
 
     def send_msg(self, msg, to=None):
-        pass
+        d = {MsgKeys.TYPE: MsgTypes.SEND_MSG, MsgKeys.MSG: msg}
+        if to:
+            d[MsgKeys.TO] = to
+        self.sock.send(b"" + json.dumps(d))
 
     def get_all_clients(self):
-        pass
+        d = {MsgKeys.TYPE: MsgTypes.GET_ALL_CLIENTS}
+        self.sock.send(b"" + json.dumps(d))
 
     def get_all_files(self):
-        pass
+        d = {MsgKeys.TYPE: MsgTypes.GET_ALL_FILES}
+        self.sock.send(b"" + json.dumps(d))
 
     def _file_download_req(self):
-        pass
+        d = {MsgKeys.TYPE: MsgTypes.FILE_DOWNLOAD_REQ}
+        self.sock.send(b"" + json.dumps(d))
 
     def file_download(self, file_name):
-        pass
+        d = {MsgKeys.TYPE: MsgTypes.FILE_DOWNLOAD}
+        self.sock.send(b"" + json.dumps(d))
+
+
+def main():
+    client = Client()
+    client.connect("moriah", "127.0.0.1", consts.SERVER_PORT)
+
+
+if __name__ == "__main__":
+    main()
--- consts.py
+++ consts.py
@@ -1,8 +1,13 @@
 from enum import Enum
 
+BUFF_SIZE = 1024
+USERNAME_LEN = 20
+SERVER_PORT = 55000
+
 
 class MsgKeys(Enum):
     TYPE = "Type"
+    USERNAME = "username"
     MSG = "msg"
     TO = "to"
 
@@ -13,6 +18,15 @@ class MsgTypes(Enum):
     SEND_MSG = 3
     GET_ALL_CLIENTS = 4
     GET_ALL_FILES = 5
-    FILE_DOWNLOAD = 6
+    FILE_DOWNLOAD_REQ = 6
+    FILE_DOWNLOAD = 7
+    CONNECT_SUCCESS = 8
+    DISCONNECT_SUCCESS = 9
+    SEND_MSG_SUCCESS = 10
+    GET_ALL_CLIENTS_SUCCESS = 11
+    GET_ALL_FILES_SUCCESS = 12
+    FILE_DOWNLOAD_REQ_SUCCESS = 13
+    FILE_DOWNLOAD_SUCCESS = 14
+
 
 
